e3ea7ab1c4fd31b3e21008f3ee4ccf44e016c60d
IsakLundstrom <isalun-9@student.ltu.se>
2021-11-11 11:40:39 +0100

    Fix user-init 1

--- database/users-init.sql
+++ database/users-init.sql
@@ -1,5 +1,5 @@
 USE db;
 
-INSERT INTO Users (UserID, Fname, Lname, HPassword, HomeAddress) VALUES (0, Isak, Ström, 123, apa@hotmail.com);
-INSERT INTO Users (UserID, Fname, Lname, HPassword, HomeAddress) VALUES (1, Ted, Gran, 123, Ted@gran.de, Vintervägen 11);
-INSERT INTO Users (UserID, Fname, Lname, HPassword, HomeAddress) VALUES (22, Lund, Sak, 12345, LundSak, Vårv. 355);
+INSERT INTO Users (Fname, Lname, HPassword, HomeAddress) VALUES (Isak, Ström, 123, 'apa@hotmail.com');
+INSERT INTO Users (Fname, Lname, HPassword, HomeAddress) VALUES (Ted, Gran, 123, 'Ted@gran.de', Vintervägen 11);
+INSERT INTO Users (Fname, Lname, HPassword, HomeAddress) VALUES (Lund, Sak, 12345, LundSak, Vårv. 355);
