ff17e707d2305b762bc9b4be905fb3c0bea521da
IsakLundstrom <isalun-9@student.ltu.se>
2021-11-11 11:31:12 +0100

    Added users-init.sql

---
+++ database/users-init.sql
@@ -0,0 +1,5 @@
+USE db;
+
+INSERT INTO Users (UserID, Fname, Lname, HPassword, HomeAddress) VALUES (0, Isak, Ström, 123, apa@hotmail.com);
+INSERT INTO Users (UserID, Fname, Lname, HPassword, HomeAddress) VALUES (1, Ted, Gran, 123, Ted@gran.de, Vintervägen 11);
+INSERT INTO Users (UserID, Fname, Lname, HPassword, HomeAddress) VALUES (22, Lund, Sak, 12345, LundSak, Vårv. 355);
