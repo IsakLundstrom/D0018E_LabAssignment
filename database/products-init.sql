fbd37490361891acc8ccaed80e399dc090a16e29
lundsak <lundmarkisak@gmail.com>
2021-11-24 13:27:30 +0000

    Added more products

--- database/products-init.sql
+++ database/products-init.sql
@@ -5,3 +5,7 @@ INSERT INTO Products (Pname, Price, Pdesc, Picture, AmountInStock) VALUES ('Blue
 INSERT INTO Products (Pname, Price, Pdesc, Picture, AmountInStock) VALUES ('Green Pen', 3.99, 'It is a green pen, wow :D', '/images/GreenPen.PNG', 3);
 INSERT INTO Products (Pname, Price, Pdesc, Picture, AmountInStock) VALUES ('Pink Pen', 2.59, 'It is a pink pen, wow :P', '/images/PinkPen.PNG', 55);
 INSERT INTO Products (Pname, Price, Pdesc, Picture, AmountInStock) VALUES ('Lime Pen', 1.89, 'It is a Lime pen, wow :DDDDDDD', '/images/LimePen.PNG', 99);
+INSERT INTO Products (Pname, Price, Pdesc, Picture, AmountInStock) VALUES ('Orange Pen', 3.99, 'It is a Orange pen', '/images/OrangePen.PNG', 22);
+INSERT INTO Products (Pname, Price, Pdesc, Picture, AmountInStock) VALUES ('Black Pen', 2.79, 'It is a Black pen, wow :', '/images/BlackPen.PNG', 44);
+INSERT INTO Products (Pname, Price, Pdesc, Picture, AmountInStock) VALUES ('White Pen', 2.99, 'It is a White pen, wow :', '/images/WhitePen.PNG', 77);
+INSERT INTO Products (Pname, Price, Pdesc, Picture, AmountInStock) VALUES ('RGB Pen', 9.99, 'It is a RGB pen :O', '/images/RGBPen.PNG', 999);
