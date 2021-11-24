86cc5cf2cdd06a0121b18f7ee70abe9b56d8f910
lundsak <lundmarkisak@gmail.com>
2021-11-24 15:30:58 +0000

    Delete function

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -4,36 +4,50 @@ var db=require('../db'); // GET ACCESS TO DB
 
 // GET settings page. 
 router.get('/', function(req, res, next) {
-    //No userid redirect to home page
-    if(!req.session.userid){
-        res.redirect('/');
-        return;
-    }
+  //No userid redirect to home page
+  if(!req.session.userid){
+      res.redirect('/');
+      return;
+  }
 
-    var sql = `SELECT * FROM Users WHERE UserID=${req.session.userid}`;
-    db.query(sql, function (err, result) {
-        if (err) throw err;
-        res.render("settings", { UserInfo: result, session: req.session });
-    });
+  var sql = `SELECT * FROM Users WHERE UserID=${req.session.userid}`;
+  db.query(sql, function (err, result) {
+      if (err) throw err;
+      res.render("settings", { UserInfo: result, session: req.session });
+  });
 });
 
 //Post/Resister User info to the datbase
 router.post('/settingsForm', function(req, res){  
-    //Get data from the form  
-    var fName = req.body.fName; 
-    var lName = req.body.lName;
-    var password = req.body.password;
-    var address = req.body.address;
-  
-    var sql = `UPDATE Users SET Fname = '${fName}', Lname = '${lName}', HPassword = '${password}', HomeAddress = '${address}' WHERE UserID = ${req.session.userid}`;
-    // QUERY DB
-    db.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Record updated"); //For debug
-  
-      res.redirect('/settings');
-      res.end();
-    });
+  //Get data from the form  
+  var fName = req.body.fName; 
+  var lName = req.body.lName;
+  var password = req.body.password;
+  var address = req.body.address;
+
+  var sql = `UPDATE Users SET Fname = '${fName}', Lname = '${lName}', HPassword = '${password}', HomeAddress = '${address}' WHERE UserID = ${req.session.userid}`;
+  // QUERY DB
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Record updated"); //For debug
+
+    res.redirect('/settings');
+    res.end();
   });
+});
+
+router.get('/deleteAccount', function(req, res){  
+
+  var sql = `DELETE FROM Users WHERE UserID = ${req.session.userid}`;
+  // QUERY DB
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Account deleted"); //For debug
+
+    req.session.destroy();
+    res.redirect('/');
+    res.end();
+  });
+});
 
 module.exports = router;
